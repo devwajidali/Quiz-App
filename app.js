3e012827730cb33a95a4536f91c5325e8614dc65
divwajidali <dev.wajidali@gmail.com>
2026-08-18 10:50:24 +0500

    Add Logic and fetch api

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ let opt1 = document.querySelector("#opt1");
 let opt2 = document.querySelector("#opt2");
 let opt3 = document.querySelector("#opt3");
 let opt4 = document.querySelector("#opt4");
+let input1 = document.querySelector("#input1");
+let input2 = document.querySelector("#input2");
+let input3 = document.querySelector("#input3");
+let input4 = document.querySelector("#input4");
+let container = document.querySelector(".container");
+let msgCont = document.querySelector(".msg");
+let msg = document.querySelector("#msg");
+let nextBtn = document.querySelector("#next");
+let restartBtn = document.querySelector(".button");
+let scoreBoard = document.querySelector(".score-board");
+let restBtn = document.querySelector("#restart-btn");
+let scoreHolder = document.querySelector("#score");
+
+let userAns ;
+let score = 0;
+
+
+
 
 function decodeHTML(html) {
   const textarea = document.createElement("textarea");
@@ -63,18 +81,89 @@ async function getQuiz() {
             opt2.innerText = decodeHTML(incorrectAns[idx2]);
             opt3.innerText = decodeHTML(incorrectAns[idx3]);
             opt4.innerText = decodeHTML(incorrectAns[idx4]);
-            console.log(incorrectAns[1]);
+
             }
+        
+        checkAns(correctAns);
     } catch (error) {
         getQuiz();
     };
 
 }
 
+function showResult(corr,user) {
+    container.classList.add("hide");
+    restBtn.classList.add("hide");
+    msgCont.classList.remove("hide");
+    if (corr === user) {
+        msg.innerText = "Correct Answer";
+        score++;
+
+        scoreHolder.innerText = score;
+
+    } else {
+        msg.innerText = `Wrong. Correct answer is ${corr}`;
+    }
+}
 
 
+
+
+
+
+function checkAns(correctAns) {
+    input1.addEventListener("click" , () => {
+        userAns = opt1.innerText;
+        showResult(correctAns,userAns);
+    
+    });
+
+
+    input2.addEventListener("click" , () => {
+        userAns = opt2.innerText;
+        showResult(correctAns,userAns);
+    
+    });
+
+    input3.addEventListener("click" , () => {
+        userAns = opt3.innerText;
+        showResult(correctAns,userAns);
+    
+    });
+
+    input4.addEventListener("click" , () => {
+        userAns = opt4.innerText;
+        showResult(correctAns,userAns);
+    
+    });
+}
+
+function resetInput () {
+    input1.checked = false;
+    input2.checked = false;
+    input3.checked = false;
+    input4.checked = false;
+}
 
 
 window.addEventListener("load", () => {
   getQuiz();
+});
+
+
+nextBtn.addEventListener("click", () => {
+    getQuiz();
+    container.classList.remove("hide");
+    restBtn.classList.remove("hide");
+    msgCont.classList.add("hide");
+
+    resetInput();
+});
+
+restBtn.addEventListener("click", () => {
+    getQuiz();
+    score = 0 ;
+
+    scoreHolder.innerText = score;
+    resetInput();
 });
